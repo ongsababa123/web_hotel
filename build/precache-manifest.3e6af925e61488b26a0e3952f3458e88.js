41bb68f80c82eaef23e421cc18550f70aa617694
BnAk <75301993+ongsababa123@users.noreply.github.com>
2023-03-29 00:38:19 +0700

    Add files via upload

---
+++ build/precache-manifest.3e6af925e61488b26a0e3952f3458e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2ee42c0aff9305b2a2f",
+    "url": "/static/js/main.b4ecbb96.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b8b9d4731e3aedef97f7",
+    "url": "/static/js/2.4686893e.chunk.js"
+  },
+  {
+    "revision": "e893a1aba32662bc38a8f0a5ffb35492",
+    "url": "/static/media/room1.e893a1ab.jpg"
+  },
+  {
+    "revision": "196e8d16eaac1c399da995017ed2b141",
+    "url": "/static/media/img3.196e8d16.png"
+  },
+  {
+    "revision": "4ee0ad79048e7af981bd2db195ba50b1",
+    "url": "/static/media/img5.4ee0ad79.png"
+  },
+  {
+    "revision": "f0ac994448530d9f59e525ff6bd36435",
+    "url": "/index.html"
+  }
+];
